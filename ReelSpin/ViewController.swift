7bc0c3d4574ca3b30948775da298ce672e4c33bb
Mike <kmjnh2016@gmail.com>
2025-05-03 13:42:15 +0800

    Initial Commit

---
+++ ReelSpin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReelSpin
+//
+//  Created by mike liu on 2025/5/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ReelSpinTests/ReelSpinTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReelSpinTests.swift
+//  ReelSpinTests
+//
+//  Created by mike liu on 2025/5/3.
+//
+
+import Testing
+@testable import ReelSpin
+
+struct ReelSpinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
